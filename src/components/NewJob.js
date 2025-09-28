ee02b8a16198fa7184da5dc62ebd8e4335a49691
Engjell Sela <eengjellsela@gmail.com>
2025-09-28 19:22:17 -0400

    added new job logic

--- src/components/NewJob.js
+++ src/components/NewJob.js
@@ -1,7 +1,38 @@
+import { useState } from "react";
+import { supabase } from "../supabaseClient";
+
 export default function NewJob({ userID }) {
+    const [name, setName] = useState('');
+    const [hourlyRate, setHourlyRate] = useState(0);
+
+    const insertJob = async () => {
+        const { data, error } = await supabase
+        .from('job')
+        .insert(
+          { name: name, hourlyRate: hourlyRate, userFK: userID },
+        )
+        if (error) console.log(error)
+        else console.log(data)
+    };
+
     return (
         <div>
-            <p>add new job for user: {userID}</p>
+            <h2>New job</h2>
+            <div>
+                <label>Job Name</label>
+                <input type="name" onChange={(e) => setName(e.target.value)} />
+            </div>
+
+            <br />
+
+            <div>
+                <label>Hourly rate</label>
+                <input type="number" onChange={(e) => setHourlyRate(e.target.value)} />
+            </div>
+
+            <br />
+
+            <button onClick={insertJob}>Submit</button>
         </div>
     )
 }
